1be1ec603d96a7113fefd27b7cad309d24ca6f58
ElisaMavilia <elisamavilia1@gmail.com>
2024-06-16 12:30:16 +0200

    added template

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,7 +1,27 @@
 <template>
-    <div>
-
+    <section>
+        <div class="card mb-3">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="..." class="img-fluid rounded-start" alt="...">
     </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Titolo</h5>
+        <small class="card-text">Durata</small>
+        <small class="card-text">Genere</small>
+        <small class="card-text">Regia</small>
+        <small class="card-text">Recensioni</small>
+        <small class="card-text">Oraio/data</small>
+        <small class="card-text">Sala</small>
+        <small class="card-text">prezzo</small>
+      </div>
+      <button type="button" class="btn btn-danger">Trailer</button>
+      <button type="button" class="btn btn-light">Scheda film</button>
+    </div>
+  </div>
+</div>
+    </section>
 </template>
 
 <script>
