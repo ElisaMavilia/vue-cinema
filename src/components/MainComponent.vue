3ad7b9d9ef6bfb9906fb1767e1c0620b42303e23
ElisaMavilia <elisamavilia1@gmail.com>
2024-06-16 17:10:05 +0200

    added select

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,7 +1,19 @@
 <template>
     <main>
-        <div class="container ps-5 text-uppercase">
-            <h3 class="pt-5">In programmazione oggi</h3>
+        <div class=" container ps-5 text-uppercase">
+            <div class="d-flex justify-content-between align-content-center align-items-center">
+                <span><h3 class="pt-5">In programmazione oggi</h3></span>
+                <div class="text-white">
+                    <select class="form-select " aria-label="Default select example">
+                    <option selected>Seleziona la fascia oraria</option>
+                    <option value="1">Pomeriggio</option>
+                    <option value="2">Sera</option>
+                    <option value="3">Notte</option>
+                </select>
+                </div>
+                
+            </div>
+           
             <CardComponent />
         </div>
     </main>
@@ -23,6 +35,12 @@ main{
     width: 100%;
     background-color: #060b16;
     color: white;
+
+    select{
+        border: 1px solid #c40a2ff5;
+        background-color: #060b16;
+        color: white;
+    }
     
 }
 </style>
